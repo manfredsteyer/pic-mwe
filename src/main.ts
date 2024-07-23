25c5ff1b060bf7bc9a0222fafe6e36303fd277ad
Manfred Steyer <manfred.steyer@gmx.net>
2024-07-23 11:59:55 +0200

    poc

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { initializePicard } from 'picard-js/client';
+
+initializePicard({
+  feed: 
+    {
+      'nfRemote': 'http://localhost:4202/remoteEntry.json'
+    }
+});
+
+// initFederation({
+//   'nfRemote': 'http://localhost:4202/remoteEntry.json'
+// })
+//   .catch(err => console.error(err))
+//   .then(_ => import('./bootstrap'))
+//   .catch(err => console.error(err));
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
